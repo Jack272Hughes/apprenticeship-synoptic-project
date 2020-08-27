e77e678473fe1c2e9cf286292c2df7a685924eda
jack272hughes <jack272hughes@gmail.com>
2020-08-27 17:41:29 +0100

    Remove MongoDB client from server
    Cleaned up server from initial commit

--- backend/server.js
+++ backend/server.js
@@ -1,25 +1,6 @@
 const app = require("express")();
 const upload = require("multer")();
 const cors = require("cors");
-const assert = require("assert");
-
-const { url, dbName } = require("./config.json");
-
-const client = new require("mongodb").MongoClient(url, {
-  useUnifiedTopology: true
-});
-client
-  .connect()
-  .then(client => {
-    console.log("Connection Successful!");
-
-    const db = client.db(dbName);
-    db.collection("users")
-      .findOne({ username: "Username" })
-      .then(result => console.log(result._id.toString()))
-      .catch(console.error);
-  })
-  .catch(console.error);
 
 app.use(upload.array());
 app.use(
@@ -33,11 +14,4 @@ app.get("/quizes", (req, res) => {
   res.end();
 });
 
-// app.listen(8080);
-
-// db.collection("users").insertOne({
-//   username: "Username",
-//   salt: "123abc",
-//   password:
-//     "96634cddae74babd0e48403d1ea1eb3c9961151d8bd39611e837bfc015431e26"
-// });
+app.listen(8080);
